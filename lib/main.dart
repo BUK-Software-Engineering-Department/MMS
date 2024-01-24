2a7972b8eec25e93e1c9111abce8f3037e826960
Jamilu Salisu <jamilusalis@gmail.com>
2024-01-24 10:39:46 +0100

    add responsive package

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:mms/screens/signin.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 //import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  await ScreenUtil.ensureScreenSize();
   runApp(const MyApp());
 }
 
