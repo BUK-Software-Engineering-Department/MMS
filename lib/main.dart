d604521eaef03ec70777758dd7d95359fb8ee501
Jamilu Salisu <jamilusalis@gmail.com>
2024-01-30 11:38:43 +0100

    firebase intergration

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:mms/screens/signin.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-//import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() async {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await ScreenUtil.ensureScreenSize();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
