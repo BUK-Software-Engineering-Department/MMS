21665eb8188f2758297ffd1b01aa9a9855b1606f
hauwajibrilibrahim <hauwajibrilibrahim@gmail.com>
2024-02-23 06:30:37 +0100

    added a drawer menu & medication history page

--- lib/ui/pages/medication_history.dart
+++ lib/ui/pages/medication_history.dart
@@ -1,13 +1,11 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:get/get.dart';
 import 'package:mms/controllers/medicine_controller.dart';
 import 'package:mms/models/medicine.dart';
-import 'package:mms/ui/theme.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:mms/ui/widgets/menu.dart';
 import '../size_config.dart';
-import '../widgets/med_card.dart';
+import '../widgets/medcard.dart';
 
 class MedicationHistory extends StatelessWidget {
   const MedicationHistory({super.key});
@@ -19,86 +17,7 @@ class MedicationHistory extends StatelessWidget {
     return Scaffold(
       appBar: _buildAppBar(context),
       backgroundColor: context.theme.colorScheme.background,
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            DrawerHeader(
-              decoration: const BoxDecoration(
-                  gradient: LinearGradient(
-                      begin: Alignment.topLeft,
-                      end: Alignment.bottomRight,
-                      colors: [
-                    Color.fromARGB(255, 28, 147, 245),
-                    Color.fromARGB(255, 145, 32, 165)
-                  ])),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      GestureDetector(
-                        /*onTap: () {
-                          ThemeService().switchTheme();
-                          notifyHelper.displayNotification(
-                            title: "Theme Changed",
-                            body: Get.isDarkMode
-                                ? "Light theme activated."
-                                : "Dark theme activated",
-                          );
-                        },*/
-                        child: Icon(
-                            Get.isDarkMode
-                                ? Icons.wb_sunny
-                                : Icons.shield_moon,
-                            color: Get.isDarkMode
-                                ? Colors.white
-                                : darkGreyClr),
-                      ),
-                      const CircleAvatar(
-                        radius: 16,
-                        backgroundImage: AssetImage("images/logo.png"),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 20),
-                  const Text(
-                    'MediGuardian',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                      fontStyle: FontStyle.italic,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            ListTile(
-              title: const Text('Home'),
-              onTap: () {
-                Navigator.pushNamed(context, '/home');
-              },
-            ),
-            ListTile(
-              title: const Text('Medication History'),
-              onTap: () {
-                Navigator.pushNamed(context, '/history');
-              },
-            ),
-            ListTile(
-              title: const Text('Logout'),
-              onTap: () {
-                FirebaseAuth.instance.signOut().then((value){
-                  print("Signed Out");
-                  Navigator.pushNamed(context, '/');
-                });
-              },
-            ),
-          ],
-        ),
-      ),
+      drawer:  MyMenu(),
       body: _buildMedicineList(medicineController),
     );
   }
@@ -112,6 +31,7 @@ class MedicationHistory extends StatelessWidget {
 
   Widget _buildMedicineList(MedicineController medicineController) {
     return Expanded(
+      flex: 1,
       child: Obx(
         () => ListView.builder(
           scrollDirection: Axis.vertical,
@@ -135,14 +55,13 @@ class MedicationHistory extends StatelessWidget {
         horizontalOffset: 300.0,
         child: FadeInAnimation(
           child: GestureDetector(
-            child: MedicineCard(medicine),
+            child: MedicineCards(medicine),
           ),
         ),
       ),
     );
   }
 }
-
 
 
   
