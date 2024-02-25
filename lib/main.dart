e288e41aee5692b5b04e5fb3c9862874ea43726f
hauwajibrilibrahim <hauwajibrilibrahim@gmail.com>
2024-02-26 00:06:05 +0100

    notifications enabled

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
-import 'package:mms/db/db_helper.dart';
 import 'package:mms/services/theme_services.dart';
 import 'package:mms/ui/pages/home_page.dart';
+import 'package:mms/ui/theme.dart';
 import 'package:mms/ui/pages/medication_history.dart';
 import 'package:mms/ui/pages/signup.dart';
 import 'package:mms/ui/pages/signin.dart';
-import 'package:mms/ui/theme.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'db/db_helper.dart';
 
+//future
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
@@ -22,17 +23,17 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
       theme: Themes.light,
       darkTheme: Themes.dark,
-      themeMode: ThemeService().theme,
+      themeMode: ThemeServices().theme,
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       //home: const HomePage(),
-      //navigatorKey: navigationKey,
       routes: Map<String, WidgetBuilder>.from({
         '/': (context) => const SignIn(),
         '/signup': (context) => const SignUpScreen(),
---
+++ lib/ui/pages/notification_screen.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../theme.dart';
+
+class NotificationScreen extends StatefulWidget {
+  const NotificationScreen({Key? key, required this.payload}) : super(key: key);
+
+  final String payload;
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _NotificationScreenState createState() => _NotificationScreenState();
+}
+
+class _NotificationScreenState extends State<NotificationScreen> {
+  String _payload = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _payload = widget.payload;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // ignore: deprecated_member_use
+      backgroundColor: context.theme.backgroundColor,
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () => Get.back(),
+          icon: Icon(
+            Icons.arrow_back_ios,
+            color: Get.isDarkMode ? Colors.white : darkGreyClr,
+          ),
+        ),
+        elevation: 0,
+        // ignore: deprecated_member_use
+        backgroundColor: context.theme.backgroundColor,
+        title: Text(
+          _payload.toString().split('|')[0],
+          style: TextStyle(color: Get.isDarkMode ? Colors.white : darkGreyClr),
+        ),
+        centerTitle: true,
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+              margin: const EdgeInsets.symmetric(horizontal: 30),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30), color: primaryClr),
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+              Text(
+                'Hey there!',
+                style: TextStyle(
+                    fontSize: 26,
+                    fontWeight: FontWeight.w900,
+                    color: Get.isDarkMode ? Colors.white : const Color.fromARGB(255, 211, 202, 202)),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Text(
+                'It\'s time to take your medication',
+                style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w300,
+                    color: Get.isDarkMode ? Colors.grey[100] : Color.fromARGB(255, 236, 227, 227)),
+              ),
+            ],
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
